b151511c061aa1d5e9c428f487e48b3965a90c26
Sunitha-Oneteam <sunitha.oneteamsolutions@gmail.com>
2025-01-08 13:27:39 +0530

    second commit

--- DemoDb/settings.py
+++ DemoDb/settings.py
@@ -77,11 +77,8 @@ WSGI_APPLICATION = 'DemoDb.wsgi.application'
 
 DATABASES = {
     'default': {    
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':  'db1'  ,
-        'USER':"root",
-        'PASSWORD':"root",
-        'HOST':"localhost"  
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':  BASE_DIR/'db.sqlite3'  ,  
     }
 }
 
